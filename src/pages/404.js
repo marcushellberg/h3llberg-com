cc8369c60b16b6ae41051ab32c10eb6a17693987
Marcus Hellberg <marcus@vaadin.com>
2019-08-18 20:01:29 -0700

    forgot this

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ class NotFoundPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" location={location} />
+        <SEO title="404: Not Found" location={this.props.location} />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
